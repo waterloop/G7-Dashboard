48b8eec87f8dd4a19ca1b49bf2e1df74c9d60ae7
Kevin-Wei-KW <69333576+Whatthehacky@users.noreply.github.com>
2025-02-21 17:21:56 -0500

    Added comments

--- dashboard/src/renderer/src/App.tsx
+++ dashboard/src/renderer/src/App.tsx
@@ -8,12 +8,19 @@ function App(): JSX.Element {
   return (
     <>
       <div className="grid-container">
+        {/* This is box number one */}
         <Box title="Box 1" description="Data 1" />
+        {/* This is box number two */}
         <Box title="Box 2" description="Data 2" />
+        {/* This is box number three */}
         <Box title="Box 3" description="Data 3" />
+        {/* This is box number one */}
         <Box title="Box 4" description="Data 4" />
+        {/* This is box number one */}
         <Box title="Box 5" description="Data 5" />
+        {/* This is box number one */}
         <Box title="Box 6" description="Data 6" />
+        {/* This is box number one */}
         <Box title="Box 7" description="Data 7" className="stretched" />
       </div>
     </>
